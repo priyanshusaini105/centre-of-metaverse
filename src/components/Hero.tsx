d73d4bfc9f2a2bad7fbfb2e718a700a7c29433a2
PRIYANSHU SAINI <priyanshusaininew@gmail.com>
2023-10-19 10:18:21 +0000

    Create Hero background and add headset image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,18 @@
 "use client";
+import Image from 'next/image'
 
 export const Hero = () => {
-  return <section className="bg-primary-400">fuugv</section>;
+  return <section className="hero-bg bg-primary-400 flex items-center flex-col">
+    <span className="uppercase text-6xl my-6 text-gray-50">Experience the</span>
+    <br/>
+    <span className="uppercase text-8xl text-secondary">Metaverse</span>
+  <Image
+  src="/img/headset.png"
+  width={1000}
+  height={300}
+  alt="Metaverse Headset"
+  className="w-[64rem] h-[35.27rem]"
+  />
+
+  </section>;
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,18 +7,18 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors:{
-      primary:{
-        '100':'#f4f3f3',
-        '200':'#84639b',
-        '300':'#190c25',
-        '400':'#0c0118',
-        '500':'#0d0319',
-        '600':'#0d021a',
-      },
-      secondary:"#960cb6"
-    },
     extend: {
+      colors:{
+        primary:{
+          '100':'#f4f3f3',
+          '200':'#84639b',
+          '300':'#190c25',
+          '400':'#0c0118',
+          '500':'#0d0319',
+          '600':'#0d021a',
+        },
+        secondary:"#960cb6"
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
